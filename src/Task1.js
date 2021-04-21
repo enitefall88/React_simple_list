51ea1a281155f69888af2976d8464abfd1143e97
Anton <enitefall81@gmail.com>
2021-04-21 15:37:09 -0700

    Add Switching between elements

--- src/Task1.js
+++ src/Task1.js
@@ -1,11 +1,10 @@
-
 import React from "react"
 
 export default function Task1() {
   let query = (new URL(document.location)).searchParams
   let c = Number(query.get("c")) || 0
   return <div className="p-3">
-    <DisplayLi  xs={[
+    <DisplayLi c={c}  xs={[
     "First Item",
     "Second Item",
     "Third Item",
@@ -15,11 +14,11 @@ export default function Task1() {
   </div>
 }
 
-function DisplayLi({xs}) {
+function DisplayLi({c, xs}) {
   return <ul className="list-group">
-    {xs.map(el =>
-        <a>
-          <li className="list-group-item active"> {el}</li>
+    {xs.map((el,i) =>
+        <a key={i} href={`?c=${i}`} className={`list-group-item list-group-item-action ${i == c ? "active" : ""}`}>
+  {el}
         </a>)}
   </ul >
 }
