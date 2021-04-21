d1b65afa2218593764dcc39b7b9ede3bdbae4839
Anton <enitefall81@gmail.com>
2021-04-21 14:15:16 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return  <div>
+    <DisplayGroup xs = {listGroup}/>
     </div>
-  );
+
+}
+
+let listGroup = [
+    "First Item",
+    "Second Item",
+    "Third Item",
+    "Fourth Item",
+    "Fifth Item"]
+
+
+function DisplayGroup({xs}) {
+  return <ul className="list-group">
+    {xs.map(el =>
+        <a>
+          <li className="list-group-item"> {el}</li>
+        </a>)}
+  </ul >
 }
 
 export default App;
